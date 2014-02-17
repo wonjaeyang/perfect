f51013e21ecabec0501baee8fa771e56468a265d
WonJae Yang <WonJaeYang@29-3-41-155-wireles1x.bu.edu>
2014-02-16 20:50:24 -0500

    Initial Commit

---
+++ perfect/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  perfect
+//
+//  Created by WonJae Yang on 2/16/14.
+//  Copyright (c) 2014 WonJae Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
